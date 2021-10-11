3690054493fa4a17fe35e4d1e916df2a39551048
Gary Tyler <mail@garytyler.com>
2021-10-11 18:09:20 +0000

    Track ./install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install docker
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install NVIDIA container toolkit
+distribution=$(
+    . /etc/os-release
+    echo $ID$VERSION_ID
+)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
