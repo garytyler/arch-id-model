e7588f238f6b3d1061b39beb7dde2b522c1cdd63
Gary Tyler <mail@garytyler.com>
2021-10-25 23:22:54 -0400

    Try disabling some volume mappings

--- Dockerfile
+++ Dockerfile
@@ -5,18 +5,19 @@ FROM tensorflow/tensorflow:2.6.0-gpu-jupyter AS train-stage
 WORKDIR /workspace/
 
 # Create non-root user
-ARG USER=${USER}
+# ARG USER=${USER}
+ARG USERNAME=${USERNAME}
 ARG USER_UID=1000
 ARG USER_GID=${USER_UID}
-RUN !(id -u "${USER}") 1>/dev/null 2>&1 \
-    && groupadd -f --gid ${USER_GID} ${USER} \
-    && useradd --uid ${USER_UID} --gid ${USER_GID} -m ${USER}
+RUN !(id -u "${USERNAME}") 1>/dev/null 2>&1 \
+    && groupadd -f --gid ${USER_GID} ${USERNAME} \
+    && useradd --uid ${USER_UID} --gid ${USER_GID} -m ${USERNAME}
 
 # Add sudo
 RUN apt-get update \
     && apt-get install --no-install-recommends -y sudo \
-    && echo ${USER} ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/${USER} \
-    && chmod 0440 /etc/sudoers.d/${USER}
+    && echo ${USERNAME} ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/${USERNAME} \
+    && chmod 0440 /etc/sudoers.d/${USERNAME}
 
 # Upgrade pip and install project dependencies from pip
 RUN pip install --upgrade pip
@@ -26,7 +27,7 @@ RUN pip install -U \
     tensorboard-plugin-profile==2.5.0
 
 # Set as non-root user
-USER ${USER}
+USER ${USERNAME}
 
 # Build development environment
 FROM train-stage AS dev-stage
@@ -46,4 +47,4 @@ RUN pip install -U \
     mypy==0.910
 
 # Set as non-root user
-USER ${USER}
+USER ${USERNAME}
--- run.sh
+++ run.sh
@@ -4,27 +4,27 @@ set -e
 
 echo $@
 THIS_DIR=$(realpath $(dirname $0))
-LOCAL_DATASET_DIR=$(realpath "${THIS_DIR}/dataset")
-DATASET_SOURCE_DIR=${DATASET_SOURCE_DIR:-${LOCAL_DATASET_DIR}}
+# LOCAL_DATASET_DIR=$(realpath "${THIS_DIR}/dataset")
+# DATASET_SOURCE_DIR=${DATASET_SOURCE_DIR:-${LOCAL_DATASET_DIR}}
 
-if [[ ! -d "$DATASET_SOURCE_DIR" ]]; then
-    echo "Path to an existing source dataset is required as an argument. Dataset dir should have structure root/classes/images."
-    exit
-fi
+# if [[ ! -d "$DATASET_SOURCE_DIR" ]]; then
+#     echo "Path to an existing source dataset is required as an argument. Dataset dir should have structure root/classes/images."
+#     exit
+# fi
 
 IMAGE_NAME=arch-recognizer/trainings
 docker build \
     --tag ${IMAGE_NAME} \
     --target=train-stage \
-    --build-arg="USER=$(whoami)" \
+    --build-arg="USERNAME=$(whoami)" \
     -- ${THIS_DIR}/
 
 RUN_COMMAND="python -m arch_recognizer $@"
 docker run \
     --runtime=nvidia \
     --publish=6006:6006 \
-    --volume="${THIS_DIR}/:/workspace/" \
-    --volume="${DATASET_SOURCE_DIR}/:/workspace/dataset/:ro" \
+    --volume="${THIS_DIR}:/workspace/" \
+    \
     --env "NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-1,2}" \
     --env "NVIDIA_DRIVER_CAPABILITIES=compute,utility" \
     --env "CUDA_DEVICE_ORDER=PCI_BUS_ID" \
@@ -33,3 +33,4 @@ docker run \
     --env "TF_CPP_MIN_LOG_LEVEL=${TF_CPP_MIN_LOG_LEVEL:-2}" \
     --rm -- ${IMAGE_NAME} \
     /bin/bash -c "${RUN_COMMAND}"
+# --volume="${DATASET_SOURCE_DIR}:/workspace/dataset/" \
